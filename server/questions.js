6d0c3e5c5eabf73cb708287c5ee091ea2f10fdb5
askarpom <askarpom@mcmaster.ca>
2025-06-09 13:38:22 -0400

    questions fix

--- server/questions.js
+++ server/questions.js
@@ -1,41 +1,92 @@
 module.exports = [
   {
     text: "What is the average weight of a newborn?",
-    options: ["12 months", "Saint West", "Ankles", "7.5 pounds"],
+    options: ["8 pounds", "6.5 pounds", "7 pounds", "7.5 pounds"],
     correct: "7.5 pounds"
   },
   {
     text: "How many diapers does a baby use in the first year?",
-    options: ["Around 2,500", "Until 1 year old", "After 3 months", "38 weeks"],
+    options: ["Around 2,500", "Around 1,500", "Around 3,500", "Around 4,500"],
     correct: "Around 2,500"
   },
   {
     text: "At what age do babies typically smile for the first time?",
-    options: ["In a car seat", "36 weeks", "9 months", "6–8 weeks"],
+    options: ["4-8 weeks", "6-8 weeks", "8-10 weeks", "10-12 weeks"],
     correct: "6–8 weeks"
   },
   {
     text: "How long does an average pregnancy last?",
-    options: ["Calf", "5 pounds", "40 weeks", "On their side"],
+    options: ["36 weeks", "38 weeks", "40 weeks", "45 weeks"],
     correct: "40 weeks"
   },
   {
     text: "How much does the average pregnant woman’s blood volume increase?",
-    options: ["blue", "2–3 months", "50%", "25%"],
+    options: ["30%", "40%", "50%", "60%"],
     correct: "50%"
   },
   {
     text: "At what age do most babies begin teething?",
-    options: ["After 12 months", "green", "10 pounds", "Around 6 months"],
+    options: ["After 12 months", "Around 8 months", "After 3 months", "Around 6 months"],
     correct: "Around 6 months"
   },
   {
     text: "What is the average age for a baby’s first steps?",
-    options: ["Around 4 months", "8–10 months", "12 months", "On their stomach"],
+    options: ["6 months", "8 months", "10 months", "12 months"],
     correct: "12 months"
   },
   {
     text: "At what age do babies usually sleep through the night?",
-    options: ["10 months", "C]()
-
-
+    options: ["7-9 months", "8-10 months", "6-8 months", "4–6 months"],
+    correct: "4–6 months"
+  },
+  {
+    text: "At what age do babies typically start clapping?",
+    options: ["6 months", "8 months", "9 months", "12 months"],
+    correct: "9 months"
+  },
+  {
+    text: "What is the safest sleeping position for a newborn?",
+    options: ["On their back", "On their belly", "On their side"],
+    correct: "On their back"
+  },
+  {
+    text: "How long should babies ride rear-facing in a car seat?",
+    options: ["Until 12 months", "Until at least 2 years old", "Until 6 years old"],
+    correct: "Until at least 2 years old"
+  },
+  {
+    text: "When can babies safely drink water?",
+    options: ["After 4 months", "After 6 months", "After birth"],
+    correct: "After 6 months"
+  },
+  {
+    text: "What is the name of Kim Kardashian and Kanye West’s first child?",
+    options: ["Blu Ivy", "Kulture", "Stormy", "Precious"],
+    correct: "North"
+  },
+  {
+    text: "What is a baby kangaroo called?",
+    options: ["Joey", "Lamb", "Pollywog", "Smolt"],
+    correct: "Joey"
+  },
+  {
+    text: "What is the first sense a baby develops in the womb?",
+    options: ["Smell", "Vision", "Hearing", "Touch"],
+    correct: "Touch"
+  },
+  {
+    text: "What is the first thing a baby recognizes visually?",
+    options: ["Light", "Faces", "Colours"],
+    correct: "Faces"
+  },
+  {
+    text: "How long does it take for the fingerprints to appear on a baby’s fingers?",
+    options: ["3 months", "6 months", "9 months", "12 months"],
+    correct: "6 months"
+  },
+  {
+    text: "Which is the colour that babies recognise first when their colour vision begins to develop?",
+    options: ["red", "blue", "green", "yellow"],
+    correct: "red"
+  }
+];
